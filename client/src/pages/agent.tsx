813f953a84f1352f59e93cb9f09559e96643034f
Jingu Kang <jinguk0816@gmail.com>
2023-02-18 18:05:23 -0800

    create components and necessary pages for the dashboard

---
+++ client/src/pages/agent.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Agents = () => {
+    return (
+        <div>agent</div>
+    )
+}
+
+export default Agents;
---
+++ client/src/pages/all-properties.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AllProperties = () => {
+    return (
+        <div>all-properties</div>
+    )
+}
+
+export default AllProperties;
