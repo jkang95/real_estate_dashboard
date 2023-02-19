4d9d9c418e268f0d44f841064345319a4020502f
Jingu Kang <jinguk0816@gmail.com>
2023-02-18 18:05:52 -0800

    clean up by collecting components into 1 file and export as 1 file

---
+++ client/src/components/index.ts
@@ -0,0 +1,12 @@
+import PieChart from "./charts/PieChart";
+import PropertyReferrals from "./charts/PropertyReferrals";
+import TotalRevenue from "./charts/TotalRevenue";
+import CustomButton from "./common/CustomButton";
+import Form from "./common/Form";
+import Profile from "./common/Profile";
+import PropertyCard from "./common/PropertyCard";
+import TopAgent from "./home/TopAgent";
+
+export {
+    PieChart, PropertyReferrals, TotalRevenue, CustomButton, Form, Profile, PropertyCard, TopAgent
+};
---
+++ client/src/pages/index.ts
@@ -0,0 +1,13 @@
+import Home from './home';
+import AgentProfile from './agent-profile';
+import Agents from './agent';
+import CreateProperty from './create-property';
+import AllProperties from './all-properties';
+import { Login } from './login';
+import MyProfile from './my-profile';
+import PropertyDetails from './property-details';
+import EditProperty from './edit-property';
+
+export {
+    AgentProfile, Agents, Home, CreateProperty, AllProperties, Login, MyProfile, PropertyDetails, EditProperty
+};
